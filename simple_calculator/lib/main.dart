9347259b0999d5fdad5bf90a46c64fed49bdcd4e
Htun Thiha (Steve) <htunthiha.dev01@gmail.com>
2025-07-28 19:13:06 +0800

    feat(simple_calculator): Add UI for calculator
    Calculator UI logic is written with button rows and buttons
    Functions are not added yet
    .vscode is added to .gitignore

--- simple_calculator/lib/main.dart
+++ simple_calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_calculator/utils/calculator_functions.dart';
 
 void main() => runApp(IndexWidget());
 
@@ -10,13 +11,149 @@ class IndexWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('Calculator App')]
-          )
-        )
+        body: SimpleCalculatorWidget()
       ),
+    );
+  }
+}
+
+class SimpleCalculatorWidget extends StatefulWidget {
+  const SimpleCalculatorWidget({super.key});
+
+  @override
+  State<SimpleCalculatorWidget> createState() => _SimpleCalculatorWidgetState();
+}
+
+class _SimpleCalculatorWidgetState extends State<SimpleCalculatorWidget> {
+  String _previousCalculationExpression = '5+5=';
+  String _displayExpression = '25';
+  final List<String> mathOperationChars = ['+', '-', '×', '÷', '%'];
+  final List<num> numInputs = [];
+  final List<double Function(num, num)> operationInputs = [];
+  bool lastInputIsNum = false;
+  bool isEmptyExpression = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 30, right: 30, bottom: 60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          SizedBox(height: 250),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  _previousCalculationExpression,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.grey
+                  )
+                )
+              ]
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                _displayExpression,
+                style: TextStyle(
+                  fontSize: 50
+                )
+              )
+            ]
+          ),
+          SizedBox(height: 20),
+          CalculatorButtonsRow(
+            firstBtnChild: Text('AC', style: TextStyle(fontSize: 25)),
+            secondBtnChild: Text('+/-', style: TextStyle(fontSize: 25)),
+            thirdBtnChild: Text('%', style: TextStyle(fontSize: 25)),
+            fourthBtnChild: Text('÷', style: TextStyle(fontSize: 25))
+          ),
+          SizedBox(height: 20),
+          CalculatorButtonsRow(
+            firstBtnChild: Text('7', style: TextStyle(fontSize: 25)),
+            secondBtnChild: Text('8', style: TextStyle(fontSize: 25)),
+            thirdBtnChild: Text('9', style: TextStyle(fontSize: 25)),
+            fourthBtnChild: Text('x', style: TextStyle(fontSize: 25))
+          ),
+          SizedBox(height: 20),
+          CalculatorButtonsRow(
+            firstBtnChild: Text('4', style: TextStyle(fontSize: 25)),
+            secondBtnChild: Text('5', style: TextStyle(fontSize: 25)),
+            thirdBtnChild: Text('6', style: TextStyle(fontSize: 25)),
+            fourthBtnChild: Text('-', style: TextStyle(fontSize: 25))
+          ),
+          SizedBox(height: 20),
+          CalculatorButtonsRow(
+            firstBtnChild: Text('1', style: TextStyle(fontSize: 25)),
+            secondBtnChild: Text('2', style: TextStyle(fontSize: 25)),
+            thirdBtnChild: Text('3', style: TextStyle(fontSize: 25)),
+            fourthBtnChild: Text('+', style: TextStyle(fontSize: 25))
+          ),
+          SizedBox(height: 20),
+          CalculatorButtonsRow(
+            firstBtnChild: Icon(Icons.history, size: 30),
+            secondBtnChild: Text('0', style: TextStyle(fontSize: 25)),
+            thirdBtnChild: Text('.', style: TextStyle(fontSize: 25)),
+            fourthBtnChild: Text('=', style: TextStyle(fontSize: 25))
+          )
+        ]
+      )
+    );
+  }
+}
+
+class CalculatorButtonsRow extends StatelessWidget {
+  final Widget firstBtnChild;
+  final Widget secondBtnChild;
+  final Widget thirdBtnChild;
+  final Widget fourthBtnChild;
+
+  const CalculatorButtonsRow({
+    super.key,
+    required this.firstBtnChild,
+    required this.secondBtnChild,
+    required this.thirdBtnChild,
+    required this.fourthBtnChild
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CalculatorButton(btnChild: firstBtnChild, onPressed: () {}),
+        Spacer(),
+        CalculatorButton(btnChild: secondBtnChild, onPressed: () {}),
+        Spacer(),
+        CalculatorButton(btnChild: thirdBtnChild, onPressed: () {}),
+        Spacer(),
+        CalculatorButton(btnChild: fourthBtnChild, onPressed: () {})
+      ]
+    );
+  }
+}
+
+class CalculatorButton extends StatelessWidget {
+  final Widget btnChild;
+  final void Function() onPressed;
+
+  const CalculatorButton({
+    super.key,
+    required this.btnChild,
+    required this.onPressed
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        padding: WidgetStatePropertyAll(EdgeInsetsGeometry.zero),
+        fixedSize: WidgetStatePropertyAll(Size(70, 70))
+      ),
+      onPressed: onPressed,
+      child: btnChild
     );
   }
 }
